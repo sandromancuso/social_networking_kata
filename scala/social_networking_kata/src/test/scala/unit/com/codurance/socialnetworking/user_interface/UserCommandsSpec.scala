e63d6ccecaebb35451070fd616eb8015529be999
Sandro Mancuso <sandromancuso@yahoo.com>
2014-03-27 16:31:40 +0000

    Removed duplication

--- scala/social_networking_kata/src/test/scala/unit/com/codurance/socialnetworking/user_interface/UserCommandsSpec.scala
+++ scala/social_networking_kata/src/test/scala/unit/com/codurance/socialnetworking/user_interface/UserCommandsSpec.scala
@@ -11,9 +11,7 @@ class UserCommandsSpec extends UnitSpec {
 
 	val USER_POST_COMMAND = "Alice -> Hello"
 
-	"UserCommands" should "return None when there is no matching command" in {
-		val commandFactory = mock[CommandFactory]
-		val userCommands = new UserCommands(commandFactory)
+	"UserCommands" should "return None when there is no matching command" in new context {
 		given(commandFactory commandFor USER_POST_COMMAND) willReturn None
 
 		userCommands execute USER_POST_COMMAND should be(None)
@@ -21,15 +19,18 @@ class UserCommandsSpec extends UnitSpec {
 		verify(commandFactory) commandFor USER_POST_COMMAND
 	}
 
-	it should "execute a command when there is a matching one" in {
-		val commandFactory = mock[CommandFactory]
-		val postCommand = mock[Command]
-		val userCommands = new UserCommands(commandFactory)
+	it should "execute a command when there is a matching one" in new context {
 		given(commandFactory commandFor USER_POST_COMMAND) willReturn Some(postCommand)
 
 		userCommands execute USER_POST_COMMAND
 
 		verify(postCommand).execute(USER_POST_COMMAND)
+	}
+
+	trait context {
+		val commandFactory = mock[CommandFactory]
+		val postCommand = mock[Command]
+		val userCommands = new UserCommands(commandFactory)
 	}
 
 }
