32763a3ac4eaa1086882327e367c8c938000ef3c
Sandro Mancuso <sandromancuso@yahoo.com>
2014-03-27 23:32:21 +0000

    UserSpec with ignored test. Realised I need a justification for writing this test since adding a new user has no visible side effect. Ignored until there is a justification for its implementation

---
+++ scala/social_networking_kata/src/test/scala/unit/com/codurance/socialnetworking/domain/UsersSpec.scala
@@ -0,0 +1,11 @@
+package unit.com.codurance.socialnetworking.domain
+
+import unit.com.codurance.socialnetworking.UnitSpec
+
+class UsersSpec extends UnitSpec {
+
+	ignore should "create a new user when user name does not exist" in {
+
+	}
+
+}
