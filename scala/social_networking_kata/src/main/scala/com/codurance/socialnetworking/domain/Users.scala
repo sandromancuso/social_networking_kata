85678a05dff7fc270b410a5ac70d35a044f4f5a3
Sandro Mancuso <sandromancuso@yahoo.com>
2014-03-28 10:19:20 +0000

    Users: stores users and posts

--- scala/social_networking_kata/src/main/scala/com/codurance/socialnetworking/domain/Users.scala
+++ scala/social_networking_kata/src/main/scala/com/codurance/socialnetworking/domain/Users.scala
@@ -1,12 +1,23 @@
 package com.codurance.socialnetworking.domain
 
+import scala.collection.mutable
+
 class Users {
+
+	val users: mutable.HashMap[String, mutable.MutableList[String]] = mutable.HashMap()
+
 	def postsBy(user_name: String): Option[List[String]] = {
-		None
+		users.get(user_name) match {
+			case Some(posts) => Some(posts.toList)
+			case _ => None
+		}
 	}
 
-	def add(userName: String, post: String) = {
-
+	def add(user_name: String, post: String) = {
+		users.get(user_name) match {
+			case Some(posts) => posts.+=:(post)
+			case _ => users += (user_name -> mutable.MutableList(post))
+		}
 	}
 
 }
--- scala/social_networking_kata/src/test/scala/unit/com/codurance/socialnetworking/domain/UsersSpec.scala
+++ scala/social_networking_kata/src/test/scala/unit/com/codurance/socialnetworking/domain/UsersSpec.scala
@@ -1,11 +1,38 @@
 package unit.com.codurance.socialnetworking.domain
 
 import unit.com.codurance.socialnetworking.UnitSpec
+import com.codurance.socialnetworking.domain.Users
 
 class UsersSpec extends UnitSpec {
 
-	ignore should "create a new user when user name does not exist" in {
-
+	"UsersSpec" should "return no posts when the user does not exist" in new context {
+		users postsBy(NON_EXISTENT_USER) should be(None)
 	}
+
+	it should "create a new user when user name does not exist" in new context {
+		users add(NEW_USER, POST)
+
+		users.postsBy(NEW_USER).get should be(List(POST))
+	}
+
+	it should "return posts in reverse-chronological order by default" in new context {
+		users add(USER, FIRST_POST)
+		users add(USER, SECOND_POST)
+
+		val posts = users postsBy(USER)
+
+		posts.get should be(List(SECOND_POST, FIRST_POST))
+	}
+
+	trait context {
+		val users = new Users
+	}
+
+	val NON_EXISTENT_USER = "non-existent user"
+	val NEW_USER = "Alice"
+	val USER = "Bob"
+	val POST = "Hello"
+	val FIRST_POST = "First post"
+	val SECOND_POST = "Second post"
 
 }
